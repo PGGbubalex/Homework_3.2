7b39bcaaf30600f54b95d52b2d62c83938f7f7c7
Igor <99yearago99@gmail.com>
2022-09-22 18:22:28 +0300

    Homework 3, task 2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+      float a = 27.12f;
+      long b = 987678965549L;
+      byte c = 2;
+      short d = 786;
+      boolean e = false;
+      int f = -159;
+      short g = 27897;
+      byte h = 67;
+    }
+}
