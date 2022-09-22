3c1cb200c8616f700f00bdd076351886c73abece
Igor <99yearago99@gmail.com>
2022-09-22 18:30:19 +0300

    Homework 3, task 2 complete

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args){
       float a = 27.12f;
+        System.out.println(a);
       long b = 987678965549L;
+        System.out.println(b);
       byte c = 2;
+        System.out.println(c);
       short d = 786;
+        System.out.println(d);
       boolean e = false;
+        System.out.println(e);
       int f = -159;
+        System.out.println(f);
       short g = 27897;
+        System.out.println(g);
       byte h = 67;
+        System.out.println(h);
+
+        // task 3 complete
     }
 }
